39b445e40f5ed060e8594c08d71626d225f8a423
Bastian <bastian@codename-php.de>
2023-04-16 16:55:04 +0200

    Added check interface

---
+++ src/Check/CheckInterface.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/*
+ *  Copyright 2023 Bastian Schwarz <bastian@codename-php.de>.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace de\codenamephp\deploymentchecks\base\Check;
+
+use de\codenamephp\deploymentchecks\base\Result\ResultInterface;
+
+/**
+ * Describes a single check that is run by the deployment checks and returns a result
+ *
+ * @psalm-api
+ */
+interface CheckInterface {
+
+  /**
+   * Runs the check and returns the result
+   *
+   * @return ResultInterface The result of the check
+   */
+  public function run() : ResultInterface;
+}
